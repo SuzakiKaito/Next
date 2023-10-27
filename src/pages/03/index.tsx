8648c7e6bff9c673d0f6472025b7949020b5c012
SuzakiKaito <114970408+SuzakiKaito@users.noreply.github.com>
2023-10-27 16:34:34 +0900

    Next.jsで電卓

---
+++ src/pages/03/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export default function calculator(){
+  const [input, setInput] = useState(''); 
+  const [result, setResult] = useState(''); 
+
+  const bClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input));
+      } 
+      catch (error) {
+        setResult('エラー');
+      }
+    } 
+    else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } 
+    else {
+      setInput((prevInput) => prevInput + value);
+    }
+  }
+
+  return(
+    <>
+    <h1>電卓</h1>
+      <div>
+        <div className="calculator">
+          <div className="display">
+            <input type="text" value={input} readOnly />
+          </div>
+          <div className="buttons">
+            <button onClick={() => bClick('9')}>9</button>
+            <button onClick={() => bClick('8')}>8</button>
+            <button onClick={() => bClick('7')}>7</button>
+            <button onClick={() => bClick('6')}>6</button>
+            <button onClick={() => bClick('5')}>5</button>
+            <button onClick={() => bClick('4')}>4</button>
+            <button onClick={() => bClick('3')}>3</button>
+            <button onClick={() => bClick('2')}>2</button>
+            <button onClick={() => bClick('1')}>1</button>
+            <button onClick={() => bClick('0')}>0</button>
+            <button onClick={() => bClick('+')}>+</button>
+            <button onClick={() => bClick('-')}>-</button>
+            <button onClick={() => bClick('*')}>*</button>
+            <button onClick={() => bClick('/')} >/</button>
+            <button onClick={() => bClick('.')}>.</button>
+            <button onClick={() => bClick('=')}>=</button>
+            <button onClick={() => bClick('C')}>C</button>
+          </div>
+        </div>
+        <p>Result: {result}</p>
+      </div>
+    </>
+  )
+}
